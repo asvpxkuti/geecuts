9a96df57494c50bcc2e2b72db1fbaef1c4d7ff9c
asvpxmani <eroppong@gmail.com>
2018-07-07 17:15:01 -0400

    added html

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start Server
