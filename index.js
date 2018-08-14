ef800d8124adad34e1ad8a01bce505cae1fb4c3a
asvpxmani <eroppong@gmail.com>
2018-08-14 12:53:02 -0400

    updated index file

--- index.js
+++ index.js
@@ -4,30 +4,37 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
-const config = require('./config/index');
+const config = require('./config/database');
 
-mongoose.Promise = require('bluebird');
-
+// Connect To Database (NEW) But not working!!!!!!!!!! (because of secret in db.js!!!!!)
+//const db = require('./config/database');
+// Map global promise - get rid of warning
+//mongoose.Promise = global.Promise;
+// Connect to mongoose
+//mongoose.connect(db.mongoURI, {
+    //useMongoClient: true
+//})
+//.then(() => console.log('MongoDB Connected...'))
+//.catch(err => console.log(err));
 
 
 // Connect To Database (OLD CODE)
-mongoose.connect(config.database,{ useNewUrlParser: true });
+mongoose.connect(config.database, { useMongoClient: true});
 // On Connection
 mongoose.connection.on('connected', () => {
-  console.log('Connected to Database ');
+  console.log('Connected to Database '+config.database);
 });
 // On Error
 mongoose.connection.on('error', (err) => {
   console.log('Database error '+err);
 });
 
+const app = express();
 
-
-const client = require('./routes/client_route');
-const user = require('./routes/user_route');
+const users = require('./routes/users');
 
 // Port Number
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 // CORS Middleware
 app.use(cors());
@@ -37,14 +44,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended':false}));
+
 // Passport Middleware
-const app = express();
 app.use(passport.initialize());
+app.use(passport.session());
+
 require('./config/passport')(passport);
 
-app.use('/client', client);
-app.use('/user', user);
+app.use('/users', users);
+
 // Index Route
 app.get('/', (req, res) => {
   res.send('invaild endpoint');
@@ -56,5 +64,5 @@ app.get('*', (req, res) => {
 
 // Start Server
 app.listen(port, () => {
-  console.log('Servers started on port '+port);
+  console.log('Server started on port '+port);
 });
